157834c201395fe903ee0ae8191bd12734fd730e
GardenHamster <zqn260256@gamil.com>
2022-02-25 19:04:03 +0800

    环境配置

--- src/main/kotlin/PluginMain.kt
+++ src/main/kotlin/PluginMain.kt
@@ -48,44 +48,23 @@ object PluginMain : KotlinPlugin(
         //配置文件目录 "${dataFolder.absolutePath}/"
         val eventChannel = GlobalEventChannel.parentScope(this)
         eventChannel.subscribeAlways<GroupMessageEvent>{
-            //群消息
-            //复读示例
+
+            var aaa=dataFolder.absolutePath;
+
             if (message.contentToString().startsWith("复读")) {
                 group.sendMessage(message.contentToString().replace("复读", "略略略"))
             }
-            if (message.contentToString() == "hi") {
-                //群内发送
-                group.sendMessage("hi")
-                //向发送者私聊发送消息
-                sender.sendMessage("hi")
-                //不继续处理
-                return@subscribeAlways
-            }
-            //分类示例
-            message.forEach {
-                //循环每个元素在消息里
-                if (it is Image) {
-                    //如果消息这一部分是图片
-                    val url = it.queryUrl()
-                    group.sendMessage("图片，下载地址$url")
-                }
-                if (it is PlainText) {
-                    //如果消息这一部分是纯文本
-                    group.sendMessage("纯文本，内容:${it.content}")
-                }
-            }
+
+
         }
         eventChannel.subscribeAlways<FriendMessageEvent>{
-            //好友信息
-            sender.sendMessage("hi")
+
         }
         eventChannel.subscribeAlways<NewFriendRequestEvent>{
-            //自动同意好友申请
-            accept()
+
         }
         eventChannel.subscribeAlways<BotInvitedJoinGroupRequestEvent>{
-            //自动同意加群申请
-            accept()
+
         }
     }
 }
